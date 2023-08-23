3368c9eab2588bebca0291478a3f14fba844be27
scapula07 <bartholomewonogwu@yahoo.com>
2023-08-23 11:20:44 +0100

    logo

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -3,14 +3,14 @@ import logo from "../assets/samslogo.png"
 
 export default function Header() {
   return (
-     <div className='w-full flex items-center justify-between px-28 bg-white py-4 '>
+     <div className='w-full flex items-center justify-between px-28 bg-white pt-6 h-full '>
        
             <img
              src={logo}
-             className="w-56 h-28"
+             className="w-56 h-20"
              />
       
-            <div className='flex items-center space-x-10 ' style={{color:"rgba(4, 34, 105, 1)"}}>
+            <div className='flex items-center space-x-10 h-20 ' style={{color:"rgba(4, 34, 105, 1)"}}>
                 <h5 className='text-sm font-semibold  px-6 py-1 '>Home</h5>
                 <h5 className='text-sm font-semibold'>About Us</h5>
                 <h5 className='text-sm font-semibold'>Services</h5>
